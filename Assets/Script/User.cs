03162074d8eea18bb365b0871cd8191e7879a321
rlacnl <152750605+rlacnl@users.noreply.github.com>
2025-06-17 10:36:00 +0900

    userAndImage 업겟

---
+++ Assets/Script/User.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class User : MonoBehaviour
+{
+    public GameObject User;
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject = new GameObject()
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
